0ae9d53fa1cd2ac36d4252d50906b05ffcc8c918
V-Blaze <valentine6586@gmail.com>
2023-03-20 14:09:00 +0100

    Fix linter errors

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,7 +2,6 @@ class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :categories
 
-
   validates :name, presence: true, length: { maximum: 250 }
   validates :icon, presence: true, length: { maximum: 500 }
 end
