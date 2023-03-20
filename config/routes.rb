3155fe70a4c094a0049a211cb9c40b0d776f516d
V-Blaze <valentine6586@gmail.com>
2023-03-20 18:36:52 +0100

    Implement category index

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  devise_scope :user do
+    authenticated :user do
+      root 'categories#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root "home#index", as: :unauthenticated_root
+    end
+  end
 end
