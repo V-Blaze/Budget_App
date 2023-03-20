ce1222eace99a4d7392292cfc0d435d65e67fafe
V-Blaze <valentine6586@gmail.com>
2023-03-20 14:00:00 +0100

    Setup routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :expenses, only: [:index, :new, :create, :destroy]
+  resources :categories, only: [:index, :new, :create, :destroy]
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
