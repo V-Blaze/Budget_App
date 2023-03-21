b3fc7a28656a4f4b01ad7fc7b270b7e600d53996
V-Blaze <valentine6586@gmail.com>
2023-03-21 22:03:23 +0100

    Fix linter errors

--- spec/features/expenses_index_spec.rb
+++ spec/features/expenses_index_spec.rb
@@ -18,13 +18,19 @@ RSpec.feature 'Expenses#index view', type: :feature, js: true do
   end
 
   scenario "Displaying at least one expense's name" do
+    # Wait until the recipe name appears on the page
     expect(page).to have_content(@expense.name, wait: 5)
+
+    # Now make the assertion
     expect(body).to have_content(@expense.name)
   end
 
   scenario "Displaying Category's expenses total amount" do
     total_expense = @expenses.inject(0) { |sum, e| sum + e.amount }
+    # Wait until the recipe name appears on the page
     expect(page).to have_content(total_expense, wait: 5)
+
+    # Now make the assertion
     expect(body).to have_content(total_expense)
   end
 
@@ -34,10 +40,25 @@ RSpec.feature 'Expenses#index view', type: :feature, js: true do
     # Make Assertion
     expect(page).to have_css('.add-new-expense-btn')
   end
+end
 
+RSpec.feature 'Expenses#index view', type: :feature, js: true do
+  before(:each) do
+    @current_user = User.first
+    @current_user.confirm if @current_user.confirmed_at.nil?
+    @category = Category.created_by_current_user(@current_user).last
+    @expenses = Expense.joins(:categories).where(categories: { id: @category.id }).order(created_at: :desc)
+    @expense = @expenses.first
+
+    visit "#{base_url}/categories/#{@category.id}/expenses"
+    fill_in 'Email', with: @current_user.email
+    fill_in 'Password', with: '123456'
+    click_button 'Log in'
+  end
   scenario 'Clicking on the add new category button redirects to /categories/:category_id/expenses/new' do
     add_btn = page.all(:css, '.add-new-expense-btn').first
     expected_url = "#{base_url}/categories/#{@category.id}/expenses/new"
+
     add_btn.click
     expect(page).to have_current_path(expected_url)
   end
@@ -45,6 +66,7 @@ RSpec.feature 'Expenses#index view', type: :feature, js: true do
   scenario 'Clicking on the back button takes you to the home page' do
     back_btn = page.all(:css, '.back-btn').first
     expected_url = base_url.to_s
+
     back_btn.click
     expect(page).to have_current_path(expected_url)
   end
