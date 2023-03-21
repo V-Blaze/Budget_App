b5b7c0807a6f69aac57f6f78d12f01dcb310373f
V-Blaze <valentine6586@gmail.com>
2023-03-21 18:39:07 +0100

    Add capybara feature test to categories index and expenses index

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,8 +69,8 @@ RSpec.configure do |config|
 
   Capybara.register_driver :selenium_chrome_headless do |app|
     options = Selenium::WebDriver::Chrome::Options.new(
-      args: ''
-      # args: %w[headless disable-gpu no-sandbox]
+      # args: ''
+      args: %w[headless disable-gpu no-sandbox]
     )
     Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
   end
